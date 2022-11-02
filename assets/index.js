856d6f9d6473f5bf51cb299d82f3e00b2edf53e4
Arspeke8 <110306643+Arspeke8@users.noreply.github.com>
2022-11-01 22:40:40 -0700

    added two more apis for 5 day forecast and the geo api for lat and lon to work with the 5 day forecast api

--- assets/index.js
+++ assets/index.js
@@ -10,6 +10,17 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+
+
+5 day forecast
+
+https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=1c8e99a8b00221d888bd9d8829968388&units=imperial
+
+
+Geocoding to convert city name into lat and Lon
+
+https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=1c8e99a8b00221d888bd9d8829968388
+
 */
 var CityName = "";
 var APIKey = "1c8e99a8b00221d888bd9d8829968388";
